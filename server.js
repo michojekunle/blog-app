16ef5b1a79a2202bfd3dcddfcfdb2cd601ff3592
AMD <michojekunle1@gmail.com>
2023-01-11 13:58:25 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./routes')
+
+app.use('/', routes)
+
+app.listen(3000, () => {
+    console.log("App is running on port 3000")
+})
---
+++ vite-project/src/components/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+        SignUp Pa
+    </div>
+  )
+}
+
+export default SignUp
