3bcc9f36b355e9bdc934099159bd2bf4c53d0087
AMD <michojekunle1@gmail.com>
2023-01-11 14:49:30 +0100

    made changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="App bg-[#eee]">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
