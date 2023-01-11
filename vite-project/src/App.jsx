e39b46f98bbabbff22545aef3e592f414babce5e
AMD <michojekunle1@gmail.com>
2023-01-11 14:31:24 +0100

    Great project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,8 +9,8 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" target="_blank" className='text-3xl text-[#800]'>
+          Hello World
         </a>
       </div>
       <h1>Vite + React</h1>
