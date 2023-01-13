9d63a70586c5017a6f1afff8d88c5ef9623d4b3f
Pere <gpere800@gmail.com>
2023-01-13 09:06:11 -0800

    made changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const multer = require('multer');
+const upload = multer()
+const cookieParser = require('cookie-parser')
+
 const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 
 const routes = require('./routes')
 
