b496f49a174a85293f78bf425597e18947fec8b5
AMD <michojekunle1@gmail.com>
2023-02-17 17:07:56 +0100

    made changes

--- client/src/components/BlogDetail.jsx
+++ client/src/components/BlogDetail.jsx
@@ -15,7 +15,7 @@ const BlogDetail = () => {
   const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   const handleLike = () => {
-
+      
   } 
 
   useEffect(() => {
